76f58bcc0ee7465b91d0d4ab456680c0f3b5d21f
Dan Lynch <pyramation@gmail.com>
2024-10-04 13:55:47 -0700

    repo url

--- packages/create-hyperweb-app/src/constants.ts
+++ packages/create-hyperweb-app/src/constants.ts
@@ -1,1 +1,4 @@
-export const CHA_URL = 'https://github.com/hyperweb-io/create-hyperweb-app.git';
+// for when public:
+// export const CHA_URL = 'https://github.com/hyperweb-io/create-hyperweb-app.git';
+// for when private:
+export const CHA_URL = 'git@github.com:hyperweb-io/create-hyperweb-app.git';

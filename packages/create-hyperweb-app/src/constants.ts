925fbf9dde7324e11bcd4c969c5e1e85ebb76479
Dan Lynch <pyramation@gmail.com>
2024-10-21 13:44:02 +0400

    https (#8)
    Co-authored-by: Anmol <anmol1696@gmail.com>

--- packages/create-hyperweb-app/src/constants.ts
+++ packages/create-hyperweb-app/src/constants.ts
@@ -1,4 +1,1 @@
-// for when public:
-// export const CHA_URL = 'https://github.com/hyperweb-io/create-hyperweb-app.git';
-// for when private:
-export const CHA_URL = 'git@github.com:hyperweb-io/create-hyperweb-app.git';
+export const CHA_URL = 'https://github.com/hyperweb-io/create-hyperweb-app.git';

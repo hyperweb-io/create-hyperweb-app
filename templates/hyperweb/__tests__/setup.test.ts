86b75743ff63d9ba08f91d9ac9f110539d1c1ca8
Anmol1696 <anmol1696@gmail.com>
2025-02-19 16:52:07 +0530

    update test cases

--- templates/hyperweb/__tests__/setup.test.ts
+++ templates/hyperweb/__tests__/setup.test.ts
@@ -1,52 +1,27 @@
-import path from "path";
+// @ts-nocheck
+import path from 'path';
 
-import { SigningClient } from "@interchainjs/cosmos/signing-client";
-import { DirectGenericOfflineSigner } from "@interchainjs/cosmos/types/wallet";
-import { Secp256k1HDWallet } from "@interchainjs/cosmos/wallets/secp256k1hd";
-import {
-  ConfigContext,
-  generateMnemonic,
-  useChain,
-  useRegistry,
-} from "starshipjs";
+import { StargateClient } from '@cosmjs/stargate';
+
+import { ConfigContext, useChain, useRegistry } from 'starshipjs';
 
 beforeAll(async () => {
-  const configFile = path.join(__dirname, "..", "configs", "local.yaml");
+  const configFile = path.join(__dirname, '..', 'configs', 'local.yaml');
   ConfigContext.setConfigFile(configFile);
   ConfigContext.setRegistry(await useRegistry(configFile));
 });
 
-describe("Test clients", () => {
-  let client: SigningClient;
+describe('Test clients', () => {
+  let client;
 
   beforeAll(async () => {
-    const { getRpcEndpoint, chainInfo } = useChain("hyperweb");
-
-    const commonPrefix = chainInfo?.chain?.bech32_prefix;
-    const cosmosHdPath = "m/44'/118'/0'/0/0";
-
-    const directWallet = Secp256k1HDWallet.fromMnemonic(generateMnemonic(), [
-      {
-        prefix: commonPrefix,
-        hdPath: cosmosHdPath,
-      },
-    ]);
-    const directSigner = directWallet.toOfflineDirectSigner();
-
-    client = await SigningClient.connectWithSigner(
-      await getRpcEndpoint(),
-      new DirectGenericOfflineSigner(directSigner),
-      {
-        signerOptions: {
-          prefix: commonPrefix,
-        },
-      }
-    );
+    const { getRpcEndpoint } = useChain('hyperweb');
+    client = await StargateClient.connect(await getRpcEndpoint());
   });
 
-  it("check chain height", async () => {
-    const { header } = await client.getBlock(1);
+  it('check chain height', async () => {
+    const height = await client.getHeight();
 
-    expect(header.height).toBeGreaterThan(0);
+    expect(height).toBeGreaterThan(0);
   });
 });

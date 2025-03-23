8fc22f0a42c0cec0dd5fc4bca3793193cd720dda
jovonni <jovonnipharr@gmail.com>
2025-03-23 00:44:22 -0400

    chore(config): added bank, token factory, and hello contracts to build config

--- templates/hyperweb/scripts/configs.ts
+++ templates/hyperweb/scripts/configs.ts
@@ -6,8 +6,25 @@ export interface BuildConfig {
 
 export const configs: BuildConfig[] = [
   {
-    entryFile: "src/simple-state/index.ts",
-    outFile: "dist/contracts/simpleState.js",
+    entryFile: "src/hello/index.ts",
+    outFile: "dist/contracts/hello.js",
     externalPackages: ["otherpackage", "~somepackage"],
   },
+  {
+    entryFile: "src/simple-state/index.ts",
+    outFile: "dist/contracts/simple-state.js",
+    externalPackages: ["otherpackage", "~somepackage"],
+  },
+  {
+    entryFile: "src/bank/index.ts",
+    outFile: "dist/contracts/bank.js",
+    externalPackages: ["@hyperweb/bank"],
+  },
+  {
+    entryFile: "src/token-factory/index.ts",
+    outFile: "dist/contracts/token-factory.js",
+    externalPackages: ["@hyperweb/token", "@hyperweb/bank"],
+  },
+
+
 ];

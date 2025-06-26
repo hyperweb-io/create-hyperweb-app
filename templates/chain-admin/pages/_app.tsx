e5e42a09c60336c96996e6e453f7985b4eb2f996
Eason <sdqede@gmail.com>
2025-06-26 23:38:00 +1200

    upgraded interchain-kit added hyperweb chain info

--- templates/chain-admin/pages/_app.tsx
+++ templates/chain-admin/pages/_app.tsx
@@ -6,10 +6,12 @@ import { ChainProvider } from '@interchain-kit/react';
 import { chains, assetLists } from 'chain-registry';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Box, Toaster, useTheme } from '@interchain-ui/react';
+import { useMemo } from 'react';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { CustomThemeProvider, Layout } from '@/components';
 import { wallets } from '@/config';
+import { useStarshipChains } from '@/hooks';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,22 +23,52 @@ const queryClient = new QueryClient({
   },
 });
 
-function CreateCosmosApp({ Component, pageProps }: AppProps) {
+// Create a separate component for the app content
+function AppContent({ Component, pageProps }: AppProps) {
   const { themeClass } = useTheme();
+  const { data: starshipData, isLoading } = useStarshipChains();
+  console.log('starshipData', starshipData);
+
+  // Merge chain-registry and starship chains
+  const combinedChains = useMemo(() => {
+    if (starshipData?.v2.chains) {
+      return [...chains, ...starshipData.v2.chains];
+    }
+    return chains;
+  }, [starshipData]);
+
+  // Merge chain-registry and starship assetLists
+  const combinedAssetLists = useMemo(() => {
+    if (starshipData?.v2.assets) {
+      return [...assetLists, ...starshipData.v2.assets];
+    }
+    return assetLists;
+  }, [starshipData]);
+
+  // Don't render ChainProvider until starship data is loaded
+  if (isLoading) {
+    return <div>Loading chains...</div>; // or your preferred loading component
+  }
 
   return (
+    <ChainProvider chains={combinedChains} assetLists={combinedAssetLists} wallets={wallets}>
+      <Box className={themeClass}>
+        <Layout>
+          <Component {...pageProps} />
+          <Toaster position="top-right" closeButton={true} />
+        </Layout>
+      </Box>
+    </ChainProvider>
+  );
+}
+
+function CreateCosmosApp(props: AppProps) {
+  return (
     <CustomThemeProvider>
-      <ChainProvider chains={chains} assetLists={assetLists} wallets={wallets}>
-        <QueryClientProvider client={queryClient}>
-          <Box className={themeClass}>
-            <Layout>
-              <Component {...pageProps} />
-              <Toaster position="top-right" closeButton={true} />
-            </Layout>
-          </Box>
-          {/* <ReactQueryDevtools /> */}
-        </QueryClientProvider>
-      </ChainProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppContent {...props} />
+        {/* <ReactQueryDevtools /> */}
+      </QueryClientProvider>
     </CustomThemeProvider>
   );
 }
